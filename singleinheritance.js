fd8243dbf7ade80516e5b551e4814c07644d1abc
ashwathram1607 <ashwathram94@gmail.com>
2026-08-26 09:15:47 +0530

    Inheritance

---
+++ singleinheritance.js
@@ -0,0 +1,13 @@
+class Animal{
+    eat(){
+        console.log("Animal is eating");
+    }
+}
+class Dog extends Animal{
+    bark(){
+        console.log("Dog is barking");
+    }
+}
+let dog=new Dog();
+dog.eat();
+dog.bark();
